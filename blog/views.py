916064d6611ba0b8d04069635ba42eaf08b30550
chul7755 <chul7755@naver.com>
2019-08-08 17:56:40 +0900

    v5

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,5 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
+def analy(request):
+    return render(request, 'blog/analy.html', {})
